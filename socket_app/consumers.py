05e908ab0124c3d030412d67528c56f7add3e142
tschmit2 <tschmit2@uni-muenster.de>
2021-11-25 18:51:32 +0100

    erster Versuch mit richtigen Daten

--- socket_app/consumers.py
+++ socket_app/consumers.py
@@ -7,32 +7,15 @@ from django.dispatch import receiver
 from django.forms.models import model_to_dict
 import time
 
-def createModelDict():
-    appDict = []
-    count = 0
-    all_appointments = appointments.objets.all
-    for app in all_appointments:
-        appDict[count] = {
-            'id' : app.id,
-            'date' : app.date,
-            'last_name' : app.last_name,
-            'first_name': app.first_name,
-            'reserved' : app.reserved,
-            'assigned': app.assigned}
-    return appDict
-
-
-
 class WSConsumer(WebsocketConsumer):
 
     def connect(self):
         self.accept()
-        test = 0
-        self.send(json.dumps({'message' : test}))
-        #while True:
-        #    self.send(json.dumps({'message' : test}))
-        #    test += 1
-        #    time.sleep(1)
+        all_appointments = []
+        models = appointments.objects.all
+        for model in models:
+            new_data = model_to_dict(model)
+        self.send(json.dumps({'appointments' : all_appointments}))
 
 
     #@receiver(post_save, sender=appointments)
