083063178597b85aef9e1c44a5dad7a7e9476fe8
Kevin <kevinchoe11@gmail.com>
2021-11-28 17:59:07 +0100

    added consumers

--- backend/consumers.py
+++ backend/consumers.py
@@ -1,22 +1,59 @@
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
 from djangochannelsrestframework.observer import model_observer
 from djangochannelsrestframework.decorators import action
+from djangochannelsrestframework.mixins import (
+    ListModelMixin,
+    PatchModelMixin,
+    UpdateModelMixin,
+    CreateModelMixin,
+    DeleteModelMixin,
+)
 
-from backend.api.serializers import appointmentsSerializer
-from backend.models.appointments import appointments
 
-class appointmentsConsumer(GenericAsyncAPIConsumer):
-    queryset = appointments.objects.all()
-    serializer_class = appointmentsSerializer
+from backend.api.serializers import appointmentSerializer, requestSerializer, capacitySerializer
+from backend.models.appointment import appointment
+from backend.models.request import request
+from backend.models.capacity import capacity
 
-    @model_observer(appointments)
+
+# Get list of appointments, patch appointments, create appointments via JSON
+class appointmentsConsumer(GenericAsyncAPIConsumer, ListModelMixin, PatchModelMixin, CreateModelMixin):
+    queryset = appointment.objects.all()
+    serializer_class = appointmentSerializer
+
+    @model_observer(appointment)
     async def appointments_activity(self, message, observer=None, **kwargs):
         await self.send_json(message)
 
     @appointments_activity.serializer
-    def appointments_activity(self, instance: appointments, action, **kwargs):
-        return appointmentsSerializer(instance).data
+    def appointments_activity(self, instance: appointment, action, **kwargs):
+        return appointmentSerializer(instance).data
 
     @action()
     async def subscribe_to_appointment_activity(self, **kwargs):
         await self.appointments_activity.subscribe()
+
+
+
+# Request Notifications
+class requestsConsumer(GenericAsyncAPIConsumer):
+    queryset = request.objects.all()
+    serializer_class = requestSerializer
+
+    @model_observer(request)
+    async def request_activity(self, message, observer=None, **kwargs):
+        await self.send_json(message)
+
+    @request_activity.serializer
+    def request_activity(self, instance: request, action, **kwargs):
+        return requestSerializer(instance).data
+
+    @action()
+    async def subscribe_to_request_activity(self, **kwargs):
+        await self.request_activity.subscribe()
+
+
+
+class capacityConsumer(GenericAsyncAPIConsumer, CreateModelMixin, DeleteModelMixin, PatchModelMixin):
+    queryset = capacity.objects.all()
+    serializer_class = capacitySerializer
--- backend/routing.py
+++ backend/routing.py
@@ -3,5 +3,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/backend/$', consumers.appointmentsConsumer.as_asgi()),
+    re_path(r'ws/backend/appointment', consumers.appointmentsConsumer.as_asgi()),
+    re_path(r'ws/backend/request', consumers.requestsConsumer.as_asgi()),
+    re_path(r'ws/backend/capacity', consumers.capacityConsumer.as_asgi()),
 ]
