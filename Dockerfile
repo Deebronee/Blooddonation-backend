aef63a7cd0e8df48c3ac762f9ff2a89b04e5a9ec
Max Lütkemeyer <max.luetkemeyer@uni-muenster.de>
2021-11-29 17:07:20 +0100

    docker funktioniert das erste mal

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
-FROM python:3.9-alpine
+#-alpine
+FROM python:3.9
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install -r requirements.txt
 COPY . /app/
+
+RUN pip install mysqlclient
+RUN pip install channels
+RUN pip install djangorestframework
+RUN pip install markdown
+RUN pip install django-filter
+RUN pip install channels_redis
+RUN pip install djangochannelsrestframework
+
+EXPOSE 8000
+
+CMD python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- bdbackend/settings.py
+++ bdbackend/settings.py
@@ -88,17 +88,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', 
+#        'NAME': os.environ.get('DB_NAME'),
+#        'USER': os.environ.get('DB_USER'),
+#        'PASSWORD': os.environ.get('DB_PASSWORD'),
+#        'HOST': 'db',   # Or an IP Address that your DB is hosted on
+#        'PORT': '3306',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
