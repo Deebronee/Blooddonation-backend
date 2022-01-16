fd633e62e19a5356fcc49bd7182f6d28ae5a9884
lenici <91320606+lenici@users.noreply.github.com>
2022-01-16 18:24:25 +0100

    check api-key in authorization header

--- userApi/userBackend/middleware/test_middleware.py
+++ userApi/userBackend/middleware/test_middleware.py
@@ -1,21 +1,15 @@
-
-
-
-class FilterIPMiddleware:
+from django.http import HttpResponseForbidden
+import os
+class TestMiddelware:
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     
-
-    # Check if client IP is allowed
-    def process_request(self, request):
-        
-
-        allowed_ips = ['192.168.1.1', '123.123.123.123'] # Authorized ip's
-        ip = request.META.get('REMOTE_ADDR') # Get client IP
-        #if ip not in allowed_ips:
-            #raise Http403 # If user is not allowed raise Error
-        print(ip)
-        return None
-      
+    def __call__(self, request):
+        token = request.headers._store['authorization'][1]
+        if token == os.environ.get('API_KEY'):
+            response = self.get_response(request)
+            return response
+        else:
+            return HttpResponseForbidden()
