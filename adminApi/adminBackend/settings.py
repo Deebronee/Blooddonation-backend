f21ed33b70dae82b5558ea65bfc647e880ca82d7
Max Lütkemeyer <max.luetkemeyer@uni-muenster.de>
2022-03-12 19:04:00 +0100

    db change

--- adminApi/adminBackend/settings.py
+++ adminApi/adminBackend/settings.py
@@ -83,7 +83,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '/srv/blooddonation/db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
